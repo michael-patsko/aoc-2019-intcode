8c2803156257704eee72297881edbc5fde72d4c4
Michael Patsko <michael_patsko@yahoo.co.uk>
2024-01-16 12:31:16 +0000

    update program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -10,9 +10,26 @@ class Program
         IntcodeFileReader fileReader = new();
 
         int[] intcodeProgram = fileReader.ReadAndProcessIntcode(filepath);
-        IntcodeUtility.ModifyNounAndVerb(intcodeProgram, 12, 2);
-        IntcodeProcessor.ProcessIntcode(intcodeProgram);
-        int result = intcodeProgram[0];
-        Console.WriteLine($"Result at address 0 after processing intcode program: {result}");
+
+        int targetValue = 19690720;
+
+        for (int noun = 0; noun <= 99; noun++)
+        {
+            for (int verb = 0; verb <= 99; verb++)
+            {
+                int[] programCopy = IntcodeUtility.CopyIntcodeProgram(intcodeProgram);
+                IntcodeUtility.ModifyNounAndVerb(programCopy, noun, verb);
+                IntcodeProcessor.ProcessIntcode(programCopy);
+
+                int result = programCopy[0];
+                if (result == targetValue)
+                {
+                    Console.WriteLine($"Target value {targetValue} found for noun {noun} and verb {verb}.");
+                    Console.WriteLine(100 * noun + verb);
+                    return;
+                }
+            }
+        }
+        Console.WriteLine($"Target value not found.");
     }
 }
