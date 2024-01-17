fbb01b194c6867105e87c46b32036b06f6f4dd64
Michael Patsko <michael_patsko@yahoo.co.uk>
2024-01-17 16:36:49 +0000

    update day2 for better testability

--- src/Challenges/Day2.cs
+++ src/Challenges/Day2.cs
@@ -5,31 +5,22 @@ namespace IntcodeComputer.Challenges
 {
     public class Day2
     {
-        public static void RunChallenge()
+        public static int[] RunChallenge(string filePath)
         {
             IntcodeFileReader fileReader = new();
-
-            // --- Day 2 ---
-            Console.WriteLine("\n--- Day 2 ---");
-
-            string filepath = "../data/day2.txt";
-            int[] intcodeProgram = fileReader.ReadAndProcessIntcode(filepath);
+            int[] intcodeProgram = fileReader.ReadAndProcessIntcode(filePath);
             int[] programCopy;
-            int result;
+            int resultPart1;
+            int resultPart2;
 
             // --- Part 1 ---
-            Console.WriteLine("\n--- Part 1 ---");
-
             programCopy = IntcodeUtility.CopyIntcodeProgram(intcodeProgram); // Create a clone of the intcode program
             IntcodeUtility.ModifyNounAndVerb(programCopy, 12, 2);
             IntcodeProcessor.ProcessIntcode(programCopy);
 
-            result = programCopy[0];
-            Console.WriteLine($"Program output for noun 12 and verb 2: {result}");
+            resultPart1 = programCopy[0];
 
             // --- Part 2 ---
-            Console.WriteLine("\n--- Part 2 ---");
-
             // Declare target output
             int targetValue = 19690720;
 
@@ -42,16 +33,15 @@ namespace IntcodeComputer.Challenges
                     IntcodeUtility.ModifyNounAndVerb(programCopy, noun, verb); // Replace the noun and verb of the program with our current combination
                     IntcodeProcessor.ProcessIntcode(programCopy); // Process the intcode program
 
-                    result = programCopy[0]; // Retrieve our output
-                    if (result == targetValue)
+                    resultPart2 = programCopy[0]; // Retrieve our output
+                    if (resultPart2 == targetValue)
                     {
-                        Console.WriteLine($"Target value {targetValue} found for noun {noun} and verb {verb}.");
-                        Console.WriteLine($"\nOutput: {100 * noun + verb}\n");
-                        return; // Target output has been reached
+                        return [resultPart1, 100 * noun + verb]; // Target output has been reached
                     }
                 }
             }
-            Console.WriteLine($"Target value not found.");
+
+            throw new Exception("Target value not reached for any noun or verb.");
         }
     }
 }
